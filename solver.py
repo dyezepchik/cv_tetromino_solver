6b6b01a7c19120e4b1c9ea2d283090dafae94ae3
Dima Ezepchik <dyezepchik@gmail.com>
2020-12-13 23:05:46 +0200

    solver: board setup

--- solver.py
+++ solver.py
@@ -1,6 +1,85 @@
+#!/usr/bin/env python3
 
+import argparse
+import typing
+
+# Define the shapes of the single parts (no need for mirrored ones) 4x4
+# tetromino_shapes = {
+#     "I": "..X...X...X...X.",
+#     "Z": "..X..XX...X.....",  # mirrored
+#     "O": ".....XX..XX.....",
+#     "L": ".X...X...XX.....",  # mirrored
+#     "T": "..X..XX...X.....",
+# }
+# Alternative shapes definitions
+tetromino_shapes = {
+    'I': [['I', 'I', 'I', 'I']],
+    'Z': [['Z', 'Z', '.']
+          ['.', 'Z', 'Z']],
+    'S': [['.', 'S', 'S']
+          ['S', 'S', '.']],
+    'O': [['O', 'O'],
+          ['O', 'O']],
+    'L': [['L', 'L', 'L'],
+          ['L', '.', '.']],
+    'J': [['J', '.', '.'],
+          ['J', 'J', 'J']],
+    'T': [['T', 'T', 'T'],
+          ['.', 'T', '.']],
+}
+
+def rotate_clockwise(matrx: [[str]]):
+    return [list(reversed(col)) for col in zip(*matrix)]
+
+def rotate(tile_def: [[str]], rotation):
+    if rotation == 0:
+        return tile_def
+    tile_def = rotate_clockwise(tile_def)
+    if rotation == 1:
+        return tile_def
+    tile_def = rotate_clockwise(tile_def)
+    if rotation == 2:
+        return tile_def
+    tile_def = rotate_clockwise(tile_def)
+    if rotation == 3:
+        return tile_def
+
+    raise ValueError("Wrong rotation value")
 
 class TetrominoSolver:
 
-    def __init__(self, array: [str]):
-        pass
+    def __init__(self, tiles: [str], board_x: int, board_y: int):
+        self.board = ["."] * board_x * board_y
+        self.tiles = tiles
+        self.board_x = board_x
+        self.board_y = board_y
+
+    @property
+    def solution(self):
+        res = ""
+        offset = 0
+        while line:=self.board[offset:offset+self.board_x]:
+            res += "".join(line) + "\n"
+            offset += self.board_x
+        return res
+
+    def tile_fits(tile: str, rotation: int, position_x: int, position_y: int):
+
+    def solve(self):
+        solution_found = False
+        while not solution_found:
+            for tile in self.tiles:
+                pass
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='For testing from command line.')
+    parser.add_argument('tiles', metavar='N', type=str, nargs='+', help='array of tiles')
+    parser.add_argument('--board-x', help='Board size X')
+    parser.add_argument('--board-y', help='Board size Y')
+
+    args = parser.parse_args()
+
+    tetromino = TetrominoSolver(args.tiles, int(args.board_x), int(args.board_y))
+
+    print(tetromino.solution)
